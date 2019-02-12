e77a19a854bb626167dfaa837ba03d7af596cc4e
Korum <alex.korum@mail.ru>
2019-02-12 15:05:46 +0300

    Полностью написанная программа для 10 варианта задания. Подключение класса с матрицами ощусетвленно с помощью Maven зависимости.

---
+++ src/main/java/WorkMatrix.java
@@ -0,0 +1,65 @@
+import Jama.Matrix;
+
+import java.util.Scanner;
+
+public class WorkMatrix {
+    private Scanner scanner = new Scanner(System.in);
+
+    public void Test(int n){
+        test(fRet(xRet(n)), yRet(n));
+    }
+
+    private double[][] fRet(double[][] x){
+        double[][] f=new double[x.length][3];
+        for (int i=0;i<f.length;i++){
+            f[i][0]=1;
+        }
+        for (int i=0;i<x.length;i++){
+            for (int j = 0; j <x[i].length ; j++) {
+               if (j==1){
+                   f[i][j]=Math.log(x[i][j]*x[i][j]);
+                   continue;
+               }
+               if (j==2){
+                   f[i][j]=Math.log(x[i][j]);
+                   continue;
+               }
+            }
+        }
+        return f;
+    }
+
+    private double[][] xRet(int n ) {
+        double[][] x = new double[n][2];
+        for (int i=0;i<x.length;i++){
+            System.out.println("Enter x["+i+"]: ");
+            for (int j = 0; j <x[i].length ; j++) {
+                x[i][j]=scanner.nextDouble();
+            }
+        }
+        return x;
+    }
+
+    private double[] yRet(int n){
+        double[] y=new double[n];
+        System.out.println("Enter y: ");
+        for (int i=0;i<y.length;i++){
+            y[0]=scanner.nextDouble();
+        }
+        return y;
+    }
+
+    private void test(double[][] x, double[] yy) {
+        Matrix A1 = new Matrix(x);
+        A1.print(10, 2);
+        Matrix B1 = A1.transpose();
+        Matrix F1 = A1.times(B1);
+        Matrix F4 = F1.inverse();
+        Matrix F2 = F4.times(A1);
+        Matrix C = new Matrix(yy, yy.length);
+        Matrix F3 = F2.times(C);
+        double[] z = F3.getColumnPackedCopy();
+        for (int i = 0; i < 3; i++)
+            System.out.println("z[" + i + "]=" + z[i]);
+    }
+}
